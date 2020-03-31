2032441b945a54f381d24984061763ef66b2476a
KosseifiTony <tony.kosseifi@isae.edu.lb>
2020-03-31 17:10:33 +0300

    TP0

---
+++ nfp121-tp0/MyFirstClass.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class MyFirstClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyFirstClass
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private int y;
+    /**
+     * Constructor for objects of class MyFirstClass
+     */
+    public MyFirstClass(int a,int b)
+    {
+        // initialise instance variables
+        this.x = a;
+        this.y = b;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int Add()
+    {
+        // put your code here
+        return x + y;
+    }
+}
